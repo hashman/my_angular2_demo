036d892fdf6575f7dd274eccc6817036be27da2a
Hashman Lin <coosos810609@gmail.com>
2016-08-13 11:03:56 +0800

    add innerHTML 繼結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,7 @@ export class HeaderComponent implements OnInit {
 
   title = 'The Will Will Web!!';
   title_url = 'http://blog.miniasp.com/';
+  sub_title = '記載著 <b>Will</b> 在網路世界的學習心得與技術分享';
 
   constructor() { }
 
